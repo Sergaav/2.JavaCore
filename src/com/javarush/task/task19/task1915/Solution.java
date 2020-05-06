7a914b4e3c62e8e4fb972449422fa960267d9930
Sergaav <sergaav@gmail.com>
2020-05-06 10:04:13 +0300

    JavaRush

---
+++ src/com/javarush/task/task19/task1915/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task19.task1915;
+
+/* 
+Дублируем текст
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws IOException {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString();
+        System.setOut(consoleStream);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+        System.out.println(result);
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        fileOutputStream.write(result.getBytes());
+        fileOutputStream.close();
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
